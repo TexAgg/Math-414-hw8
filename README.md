a1bc1b441580b55ec6d40a0c5560143b7580c43b
Matt G <matt.gaikema@tamu.edu>
2016-04-13 00:00:07 -0500

    Import wavelets package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Math-414-hw8
-Discretizing a function.
+Discretizing a function and find the wavelet using the [wavelets](https://cran.r-project.org/web/packages/wavelets/wavelets.pdf) package.
--- main.R
+++ main.R
@@ -1,17 +1,21 @@
 #Clear previous variables
 rm(list=ls())
 
+library(wavelets)
+
 g = function(t) {
-    if (t >= 0 && t < 7/17) {
+    b = 7/17
+	if (t >= 0 && t < b) {
         return(0)
-    } else if (t >= 7/17 && t < 1) {
+    } else if (t >= b && t < 1) {
         return(1-t^2)
     } else {
         return(NA)
     }
 }
 
-t = seq(0,1,by=0.01)
+j = 7
+k = seq(0,1,by=1/(2^j-2))
 
 # Plot original function
-plot(t, lapply(t,g),ylab="g",main="original function")
+plot(k, lapply(k,g),ylab="g",main="original function")
